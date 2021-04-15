1355146b18cc92fd31c85660fde40f187b27fee7
Cherita Ousley <32004312+cheritaousley@users.noreply.github.com>
2021-04-15 17:00:02 -0500

    get react app form working

--- MusicRecommendationsApi/musicrecommendationsreact/src/App.js
+++ MusicRecommendationsApi/musicrecommendationsreact/src/App.js
@@ -1,7 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
 function App() {
-  const [songName, albumName, artistName, genre, isRecommended, price, setNewRecommendation] = useState("");
+  const [songName, setSongName]= useState("")
+  const [albumName, setAlbumName]= useState("")
+  const [artistName, setArtistName]= useState("")
+  const [genre, setGenre]= useState("")
+  const [isRecommended, setIsRecommended]= useState("")
+  const [price, setPrice] = useState("")
+  // object in current state and a function that will change it
   const [recommendations, setRecommendations] = useState([]);
 
   async function getRecommendations() {
@@ -14,9 +20,12 @@ function App() {
     e.preventDefault();
     await fetch('/api/recommendations', {
       method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
       body: JSON.stringify({ SongName: songName, AlbumName:albumName, ArtistName:artistName, Genre: genre, Price: price, isRecommended:isRecommended  })
     });
-    setNewRecommendation("");
     await getRecommendations();
   }
 
@@ -44,14 +53,15 @@ function App() {
       <header className="header">
         <h1>Recommendations</h1>
         <form onSubmit={createRecommendation}>
-          <input placeholder="What is the song name?" value={songName} onChange={(e) => setNewRecommendation(e.target.value)} />
-          <input placeholder="What is the album name?" value={albumName} onChange={(e) => setNewRecommendation(e.target.value)} />
-          <input placeholder="What is the artist name?" value={artistName} onChange={(e) => setNewRecommendation(e.target.value)} />
+          <input placeholder="What is the song name?" value={songName} onChange={(e) => setSongName(e.target.value)} />
+          <input placeholder="What is the album name?" value={albumName} onChange={(e) => setAlbumName(e.target.value)} />
+          <input placeholder="What is the artist name?" value={artistName} onChange={(e) => setArtistName(e.target.value)} />
         {/* change to dropdown */}
-          <input placeholder="What is the genre?" value={genre} onChange={(e) => setNewRecommendation(e.target.value)} />
+          <input placeholder="What is the genre?" value={genre} onChange={(e) => setGenre(e.target.value)} />
          {/* change to checkbox for bool */}
-          <input placeholder="Do you recommend it?" value={isRecommended} onChange={(e) => setNewRecommendation(e.target.value)} />
-          <input placeholder="What is the price?" value={price} onChange={(e) => setNewRecommendation(e.target.value)} />
+          <input placeholder="Do you recommend it?" value={isRecommended} onChange={(e) => setIsRecommended(e.target.value)} />
+          <input placeholder="What is the price?" value={price} onChange={(e) => setPrice(e.target.value)} />
+          <button type="submit">Add Recommendation</button>
         </form>
       </header>
       <section className="main" style={{ display: "block" }}>
